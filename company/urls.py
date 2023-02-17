9f1060879780b0904ea30cc49c421c1eff852913
quonly <amornthep.mos07@gmail.com>
2023-02-17 17:34:48 +0700

    Extra Homework

--- company/urls.py
+++ company/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
-from .views import (Homepage, about, Product, post_detail,Contact,search,addbook)
+from .views import (Homepage, about, Product, post_detail,
+                    Contact, search, addbook, book)
 
 urlpatterns = [
     path('', Homepage),
-    path('about/', about,name='about'),
-    path('product/', Product,name='product'),
-    path('blog/<int:test>',post_detail), # add argument in urls based on parameter in views.py
-    path('contact',Contact,name='contact'),
-    path('search',search,name='search'),
-    path('addbook',addbook,name='addbook')
+    path('about/', about, name='about'),
+    path('product/', Product, name='product'),
+    # add argument in urls based on parameter in views.py
+    path('blog/<int:test>', post_detail),
+    path('contact', Contact, name='contact'),
+    path('search', search, name='search'),
+    path('addbook', addbook, name='addbook'),
+    path('book', book, name='book'),
+    path('book/delete/<int:id>', book),
+    path('book/edit/<int:id>', book),
 ]
--- company/views.py
+++ company/views.py
@@ -1,31 +1,39 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Post,ProductDemo,Book
+from .models import Post, ProductDemo, Book
 from .forms import ContactForm
 from django.db.models import Q
 # Create your views here.
 # render(request,path html)
+
+
 def Homepage(request):
     # variable = Table.objects.method()
     # Query all posts
-    all_posts = Post.objects.all() # all is call query set
-    
-    return render(request,'company/home.html',{'all_posts':all_posts})
+    all_posts = Post.objects.all()  # all is call query set
 
-def post_detail(request, test): # ref with id must add argument id
-    single_post = Post.objects.get(id=test) 
-    context = {'data':single_post}
-    return render(request,'company/post-detail.html',context)
+    return render(request, 'company/home.html', {'all_posts': all_posts})
+
+
+def post_detail(request, test):  # ref with id must add argument id
+    single_post = Post.objects.get(id=test)
+    context = {'data': single_post}
+    return render(request, 'company/post-detail.html', context)
+
 
 def about(request):
-    data = ProductDemo.objects.all() # objects.all() เป็นการคิวรี่แบบ Django --> SELECT * from company_ProductDemo (SQL)
-    context = {'data':data}
-    return render(request,'company/about.html',context)
+    # objects.all() เป็นการคิวรี่แบบ Django --> SELECT * from company_ProductDemo (SQL)
+    data = ProductDemo.objects.all()
+    context = {'data': data}
+    return render(request, 'company/about.html', context)
+
 
 def Product(request):
-    data = ProductDemo.objects.all() # objects.all() เป็นการคิวรี่แบบ Django --> SELECT * from company_ProductDemo (SQL)
-    context = {'data':data}
-    return render(request,'company/product.html',context)
+    # objects.all() เป็นการคิวรี่แบบ Django --> SELECT * from company_ProductDemo (SQL)
+    data = ProductDemo.objects.all()
+    context = {'data': data}
+    return render(request, 'company/product.html', context)
+
 
 def Contact(request):
     if request.method == "POST":
@@ -36,34 +44,68 @@ def Contact(request):
             return redirect("/")
     else:
         form = ContactForm()
-    return render(request,'company/contact.html',{
-        'form' : form,
+    return render(request, 'company/contact.html', {
+        'form': form,
     })
-    
+
+
 def search(request):
     # Get the incoming query params
     search_post = request.GET.get('q')
     if search_post:
-        posts = Post.objects.filter(Q(title__icontains = search_post))
+        posts = Post.objects.filter(Q(title__icontains=search_post))
     else:
         pass
-    return render(request,'company/search.html',{
-        'posts':posts,
+    return render(request, 'company/search.html', {
+        'posts': posts,
         'search_text': search_post,
     })
-    
+
+
 def addbook(request):
     if request.method == 'POST':
         data = request.POST.copy()
-        book_title = data.get('book_title') # name in html
-        book_descripton = data.get('book_descripton')
+        book_title = data.get('book_title')  # name in html
+        book_description = data.get('book_description')
         book_price = data.get('book_price')
-        
+
         newbook = Book()
         newbook.title = book_title
-        newbook.description = book_descripton
+        newbook.description = book_description
         newbook.price = float(book_price)
         newbook.save()
-        return redirect('/')
+        return redirect('/book')
+
+    return render(request, 'company/addbook.html')
+
+
+def book(request,id=""):
+    if request.path == '/book/delete/{}'.format(id):
+        book_del = Book.objects.get(id=id)
+        book_del.delete()
+        return redirect('/book')
+    data = Book.objects.all()
+    books = []
     
-    return render(request,'company/addbook.html')
+    if request.path == '/book/edit/{}'.format(id):
+        if request.method == 'POST':
+            data = request.POST.copy()
+            book_title = data.get('book_title')  # name in html
+            book_description = data.get('book_description')
+            book_price = data.get('book_price')
+            
+            book_edit = Book.objects.get(id=id)
+            book_edit.title = book_title
+            book_edit.description = book_description
+            book_edit.price = book_price
+            book_edit.save()
+            return redirect('/book')
+    
+    for no, book in enumerate(data, 1):
+        books.append({"id": book.id, "no": no,
+                     "title": book.title, "price": book.price,"description":book.description})
+
+
+    return render(request, 'company/book.html', {
+        'books': books
+    })
